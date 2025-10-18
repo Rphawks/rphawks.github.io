d5a518e2d4c8f0e7560b5e8e8ac324e3931944b6
Rphawks <91341499+Rphawks@users.noreply.github.com>
2025-10-18 23:54:39 +0200

    testing swup js fix

--- scripts/swup.js
+++ scripts/swup.js
@@ -1,4 +1,6 @@
 import Swup from "https://unpkg.com/swup@4?module";
+import ScriptsPlugin from "https://unpkg.com/@swup/scripts-plugin@2?module";
 
 const swup = new Swup({
+    plugins: [new ScriptsPlugin()],
 });
