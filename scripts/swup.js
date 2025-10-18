fd0d0ce64d0ee0c827fb8fc08bce9cf177f86f47
Rphawks <91341499+Rphawks@users.noreply.github.com>
2025-10-19 00:51:15 +0200

    Update swup.js

--- scripts/swup.js
+++ scripts/swup.js
@@ -1,6 +1,6 @@
 import Swup from "https://unpkg.com/swup@4?module";
 import ScriptsPlugin from "https://unpkg.com/@swup/scripts-plugin@2?module";
-import HeadPlugin from '@swup/head-plugin';
+import HeadPlugin from "https://unpkg.com/@swup/head-plugin@2?module";
 
 const swup = new Swup({
     plugins: [new ScriptsPlugin(), new HeadPlugin()],
